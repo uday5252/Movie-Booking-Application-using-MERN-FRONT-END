283786bf4fb43814e32bab5f495a50eff2112c28
uday <udaykirantutor@gmail.com>
2025-02-11 18:17:13 +0530

    payment

---
+++ src/PaymentGateway.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function PaymentGateway() {
+  return (
+    <div>
+        Here the code is written to develop payment gateway!
+    </div>
+  )
+}
+
+export default PaymentGateway
